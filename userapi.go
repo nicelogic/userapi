8cc43ab937bcfb9fcd39422ea810960f3e167f97
nicelogic <358844436@qq.com>
2024-03-27 13:13:35 +0800

    update get usersinfo

--- userapi.go
+++ userapi.go
@@ -19,23 +19,41 @@ func (client *UserApiClient) Init(endpoint string) error {
 	return nil
 }
 
-func (client *UserApiClient) GetUsersJson(token string, ctx context.Context, ids []string, ) (string, error) {
+func (client *UserApiClient) GetUsersInfo(token string, ctx context.Context, ids []string) (map[string]string, error) {
 
 	req := graphql.NewRequest(`
 	query getUsers($ids: [ID!]!){
-		getUsers(ids:$ids)
+		getUsers(ids:$ids){
+			id
+			info
+		}
 	  }
 `)
 	req.Var("ids", ids)
 	req.Header.Set("authorization", "Bearer "+token)
-	var response map[string]interface{}
+	var response map[string]any
 	if err := client.client.Run(ctx, req, &response); err != nil {
-		return "", err
+		return nil, err
 	}
-	usersJson, ok := response["getUsers"].(string)
+	usersJson, ok := response["getUsers"].([]any)
 	if !ok {
-		return "", fmt.Errorf("response parse error")
+		return nil, fmt.Errorf("response[getUsers] parse error")
 	}
-	return usersJson, nil
+	usersInfo := map[string]string{}
+	for _, userJson := range usersJson {
+		userInfo, ok := userJson.(map[string]any)
+		if !ok {
+			return nil, fmt.Errorf("userJson.(map[string]string) parse error")
+		}
+		userId, ok := userInfo["id"].(string)
+		if !ok {
+			return nil, fmt.Errorf("userInfo[id] parse error")
+		}
+		userInfoJson, ok := userInfo["info"].(string)
+		if !ok {
+			return nil, fmt.Errorf("userInfo[info] parse error")
+		}
+		usersInfo[userId] = userInfoJson
+	}
+	return usersInfo, nil
 }
-
